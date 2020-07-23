6ddd5431b46cc7029c699718554906028b947d36
Nagesh Mishra <nagesh3.13@gmail.com>
2020-07-23 14:00:01 +0530

    Removing SMTP controller

--- src/main/java/com/service/mail/MailServiceApplication.java
+++ src/main/java/com/service/mail/MailServiceApplication.java
@@ -1,8 +1,15 @@
 package com.service.mail;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -21,4 +28,27 @@ class Health {
 		return "Ok";
 	}
 
+	@CrossOrigin
+	@GetMapping("/api/SMTPController/send/{body}")
+	public void sendSimpleMessage(@PathVariable String body) {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("lushdelightful.decor");
+		mailSender.setPassword(password);
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("lushdelightful.decor@gmail.com");
+		message.setTo("lushdelightful.decor@gmail.com");
+		message.setSubject("lushdelightful.decor@gmail.com");
+		message.setText(body);
+		mailSender.send(message);
+	}
+
+	@Value("${spring.mail.password}")
+	private String password;
 }
