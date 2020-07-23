3effcdba2fe349862c9d469139fa916380690337
Nagesh Mishra <nagesh3.13@gmail.com>
2020-07-23 12:49:15 +0530

    Health added

--- src/main/java/com/service/mail/MailServiceApplication.java
+++ src/main/java/com/service/mail/MailServiceApplication.java
@@ -2,12 +2,23 @@ package com.service.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MailServiceApplication.class, args);
+	}
+
+}
+
+@RestController
+class Health {
+	@GetMapping("/health")
+	public String health() {
+		return "Ok";
 	}
 
 }
